6f131a47737841a7c66bc90023e35eda9c303cf0
kicco73 <specifico73@yahoo.it>
2014-06-10 19:12:41 +0200

    Create fake_model.php

---
+++ webapp/application/models/fake_model.php
@@ -0,0 +1,33 @@
+<?php
+class Fake_model extends CI_Model {
+	var $fields = array(
+			//'id',
+			'url', 'it', 'en', 'product'
+	);
+		
+	function __construct() {
+		parent::__construct();
+	}
+	
+	function get_default() {
+		$this->db->where('product', 'basic')->limit(1);
+		$result = $this->db->get('skin')->result();
+		if($result)
+			return $result[0];
+	}
+
+	function get($skinid) {
+		$this->db->where('id', $skinid);
+		$result = $this->db->get('skin')->result();
+		if($result)
+			return $result[0];
+	}
+	
+	function list_all($player) {
+		$this->db->from("skin")->where("product", $player->product);
+		// FIXME order_by hardcoded
+		$skins = $this->db->order_by('it', 'asc')->get()->result();
+		return $skins;
+	}
+	
+}
