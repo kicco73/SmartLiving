e7cffbc94e92985c98d81643f1899efc5ea8b955
Cremesis <ivanbelluco@gmail.com>
2014-06-13 11:13:47 +0200

    Aggiunto .gitignore

--- resource_directory/obj_z1/nullradio.d
+++
@@ -1,6 +0,0 @@
-obj_z1/nullradio.o: /home/user/contiki-2.6/core/dev/nullradio.c \
- /home/user/contiki-2.6/core/./dev/nullradio.h \
- /home/user/contiki-2.6/core/./dev/radio.h
-/home/user/contiki-2.6/core/dev/nullradio.c :
- /home/user/contiki-2.6/core/./dev/nullradio.h :
- /home/user/contiki-2.6/core/./dev/radio.h :
