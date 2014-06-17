4635a245467294c987915ce619bf3871ab22346a
Enrico Carniani <enrico.carniani@gmail.com>
2014-06-17 12:09:56 +0200

    implementing refresh

--- webapp/groupA.sql
+++ webapp/groupA.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.33)
 # Database: groupA
-# Generation Time: 2014-06-14 06:12:33 +0000
+# Generation Time: 2014-06-17 10:08:28 +0000
 # ************************************************************
 
 
@@ -32,20 +32,40 @@ CREATE TABLE `Resource` (
   `currentValue` decimal(10,3) NOT NULL DEFAULT '0.000',
   `resType` enum('sensor','switch','dimmer') NOT NULL DEFAULT 'sensor',
   `unit` varchar(32) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `url` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `Resource` WRITE;
 /*!40000 ALTER TABLE `Resource` DISABLE KEYS */;
 
-INSERT INTO `Resource` (`id`, `name`, `description`, `currentValue`, `resType`, `unit`)
+INSERT INTO `Resource` (`id`, `name`, `description`, `currentValue`, `resType`, `unit`, `timestamp`, `url`)
 VALUES
-	(1,'/fan','Fan',0.000,'switch',''),
-	(2,'/light','Dimmer',0.000,'dimmer','lux'),
-	(3,'/accelerometer','Accelerometer',0.000,'sensor','?');
+	(7,'http://localhost:8888/resources/fake_rd/fan','',0.000,'switch','','2014-06-17 11:45:15',''),
+	(8,'http://localhost:8888/resources/fake_rd/light','',0.300,'dimmer','lux','2014-06-17 11:45:15',''),
+	(9,'http://localhost:8888/resources/fake_rd/accelerometer','',0.000,'sensor','m/s^2','2014-06-17 11:45:15','');
 
 /*!40000 ALTER TABLE `Resource` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+# Dump of table Sample
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Sample`;
+
+CREATE TABLE `Sample` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `resourceId` int(11) unsigned NOT NULL,
+  `value` decimal(10,3) NOT NULL DEFAULT '0.000',
+  `timeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `resourceId` (`resourceId`),
+  CONSTRAINT `sample_ibfk_1` FOREIGN KEY (`resourceId`) REFERENCES `Resource` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 
